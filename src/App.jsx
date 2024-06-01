fa06065e078ac480de92ec8be21da60a5d7943e6
Mazbauddin <mazbauddin2010@gmail.com>
2024-06-01 21:35:41 +0600

    starter pack ready

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h2 className="text-6xl text-center text-red-600">Material Tailwind</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
